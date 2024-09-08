d5d2a63b8ee6b33231b1e69b668f3c7f55c62f39
Byron Watts <btwatts@gmail.com>
2024-09-08 23:49:34 +0800

    Confused

--- pyefis/hmi/actionclass.py
+++ pyefis/hmi/actionclass.py
@@ -42,7 +42,6 @@ class ActionClass(QWidget):
     # arg = <inst name>,<inst name>,<inst name>,..:<Command>
 
     def exit(self, action):
-        print(f"exit action ({action})")  # BUGBUG DEBUG BTW
         import sys
         sys.exit(130)
 
